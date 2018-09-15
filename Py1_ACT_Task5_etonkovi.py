12e1d9684547575f924caa0e9160ce1822ccfd53
emily-tonk04 <43283239+emily-tonk04@users.noreply.github.com>
2018-09-14 22:09:12 -0400

    Add files via upload

---
+++ Py1_ACT_Task5_etonkovi.py
@@ -0,0 +1,36 @@
+# Activity 1 Task 5
+# File: Py1_ACT_Task5.py
+# Date: 13 September 2018
+# By: Nicholas Giovanni Scarfe
+# nscarfe
+# Stella Erickson
+# ericks30
+# Emily Tonkovich
+# etonkovi
+# Gautam Fotedar
+# gfotedar
+# Section: 1
+# Team: 20
+#
+# ELECTRONIC SIGNATURE
+# Nicholas Giovanni Scarfe
+# Stella Erickson
+# Emily Tonkovich
+# Gautam Fotedar
+#
+# The electronic signatures above indicate that the program
+# submitted for evaluation is the combined effort of all
+# team members and that each member of the team was an
+# equal participant in its creation. In addition, each
+# member of the team has a general understanding of
+# all aspects of the program development and execution.
+#
+# A BRIEF DESCRIPTION OF WHAT THE PROGRAM OR FUNCTION DOES
+# calculates area of a circle with given radius
+import math
+# acceptable inputs: r >= 0
+r = float(input('What is the radius?'))
+# use pi * r^2 to calculate radius
+areA = math.pi * r**2
+
+print('The area of a circle is ' ,areA, '.') 
